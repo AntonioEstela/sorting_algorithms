a7cff8b2e30dc61ed6d04fda770b96e311518a30
Antonio Estela <antonioestela73@gmail.com>
2020-06-14 18:50:56 -0500

    Added more functions

--- sort.h
+++ sort.h
@@ -21,5 +21,9 @@ typedef struct listint_s
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
 void bubble_sort(int *array, size_t size);
-
+void insertion_sort_list(listint_t **list);
+void swap_nodes(listint_t **list, listint_t *current, listint_t *B);
+void selection_sort(int *array, size_t size);
+int find_minimun(int *array, int index, int size);
+void swap(int *array, int first, int second);
 #endif /* SORT_H */
