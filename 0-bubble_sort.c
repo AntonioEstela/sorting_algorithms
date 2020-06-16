4d036da4276ce18ada94c60ac5d3c6a00740de28
Antonio Estela <antonioestela73@gmail.com>
2020-06-16 17:48:21 -0500

    Fixed bubble sort algorithm

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -10,16 +10,19 @@ void bubble_sort(int *array, size_t size)
 {
 	unsigned int i, j, tmp;
 
-	for (i = 0; i < size; i++)
+	if (array != NULL && size > 0)
 	{
-		for (j = 1; j < size - i; j++)
+		for (i = 0; i < size; i++)
 		{
-			if (array[j - 1] > array[j])
+			for (j = 1; j < size - i; j++)
 			{
-				tmp = array[j - 1];
-				array[j - 1] = array[j];
-				array[j] = tmp;
-				print_array(array, size);
+				if (array[j - 1] > array[j])
+				{
+					tmp = array[j - 1];
+					array[j - 1] = array[j];
+					array[j] = tmp;
+					print_array(array, size);
+				}
 			}
 		}
 	}
